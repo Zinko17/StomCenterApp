79311c869d1ca2d5aa10949174eac2f3b0498fb9
Zinko17 <vovazin2@gmail.com>
2021-06-29 18:24:36 +0600

    create day serializer

---
+++ stom_project/signup/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import *
+
+
+class DaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Day
+        fields = ['name']
