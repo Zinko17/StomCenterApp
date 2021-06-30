2630684f1a2b8ff3c8e7c57470c163995bd12090
Zinko17 <vovazin2@gmail.com>
2021-06-30 18:01:51 +0600

    create paths for DayViewSet and DoctorDayView

--- stom_project/signup/urls.py
+++ stom_project/signup/urls.py
@@ -2,5 +2,25 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',DayViewSet.as_view({'get':'list'}))
+    path('',DayViewSet.as_view({
+        'get':'list',
+        'post':'create'
+                                }), name='day'),
+    path('day/<int:day_id>/',DayViewSet.as_view(
+        {
+            'get':'retrieve',
+            'put':'update',
+            'delete':'destroy',
+        }), name='day_detail'),
+    path('doctor_day/',DoctorDayView.as_view({
+        'get':'list'
+    }),name='doctor_day'),
+    path('create_wd/<int:day_id>/',DoctorDayView.as_view({
+        'post':'create'
+    }),name='create_wd'),
+    path('modify_wd/<int:doctor_day_id>/',DoctorDayView.as_view({
+        'put':'update',
+        'get':'retrieve',
+    }),name='modify_wd')
+
 ]
