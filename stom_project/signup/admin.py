14d855d95c2fe11e69586d1fe40ef9eb38478ef9
Zinko17 <vovazin2@gmail.com>
2021-07-01 18:16:47 +0600

    register order model in admin panel

--- stom_project/signup/admin.py
+++ stom_project/signup/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from .models import *
 
+
 class DayAdmin(admin.ModelAdmin):
     list_display = ['name']
-admin.site.register(Day,DayAdmin)
+
+
+admin.site.register(Day, DayAdmin)
+
 
 class DayDoctorAdmin(admin.ModelAdmin):
-    list_display = ['doctor','day']
-admin.site.register(DoctorDay,DayDoctorAdmin)
+    list_display = ['doctor', 'day']
+
+
+admin.site.register(DoctorDay, DayDoctorAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'doctor', 'day', 'date_created']
+    readonly_fields = ['date_created']
+
+
+admin.site.register(Order, OrderAdmin)
