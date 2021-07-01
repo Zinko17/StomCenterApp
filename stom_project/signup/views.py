974687530760c4a6088262c0208ab4e3ce710c8b
Zinko17 <vovazin2@gmail.com>
2021-07-01 18:22:21 +0600

    create class OrderView with methods:create,list,update,retrieve and destroy

--- stom_project/signup/views.py
+++ stom_project/signup/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from .serializers import *
-from .permissions import IsSuperUser, DoctorDayPermission
+from .permissions import IsSuperUser, DoctorDayPermission, ClientPermission
 
 
 class DayViewSet(ViewSet):
@@ -60,8 +60,64 @@ class DoctorDayView(ViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=202)
+        return Response(serializer.errors)
 
     def retrieve(self, request, doctor_day_id):
         work_day = DoctorDay.objects.get(id=doctor_day_id)
         serializer = DoctorDaySerializer(work_day)
         return Response(serializer.data, status=200)
+
+
+class OrderViewSet(ViewSet):
+    permission_classes = [ClientPermission]
+    serializer_class = OrderSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        client = request.user
+        if serializer.is_valid():
+            doctor_id = serializer.data.get('doctor')
+            day_id = serializer.data.get('day')
+            try:
+                work_day = DoctorDay.objects.get(doctor=doctor_id, day=day_id)
+                day = Day.objects.get(id=day_id)
+                doctor = User.objects.get(id=doctor_id)
+                if work_day.status == 'free':
+                    order = Order.objects.create(client=client, doctor=doctor, day=day)
+                    work_day.status = 'reserved'
+                    work_day.save()
+                    return Response(serializer.data, status=201)
+                return Response('Это время уже занято!', 400)
+            except DoctorDay.DoesNotExist:
+                return Response('Врач в такой день не работает!', status=404)
+        return Response(serializer.errors, status=400)
+
+    def list(self, request):
+        group = request.user.groups.all()[0].name
+        user = request.user
+        if group == 'client':
+            orders = Order.objects.filter(client=user)
+        elif group == 'doctor':
+            orders = Order.objects.filter(doctor=user)
+        serializer = OrderDisplaySerializer(orders, many=True)
+        return Response(serializer.data)
+
+    # TODO cannot update on day when doctor doesnot work!
+    def update(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        serializer = self.serializer_class(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=202)
+        return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        serializer = self.serializer_class(order)
+        return Response(serializer.data, status=200)
+
+    def destroy(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        o_id = order.id
+        order.delete()
+        return Response(f'{o_id} successfully deleted!', status=204)
