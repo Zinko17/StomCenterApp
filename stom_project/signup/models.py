019beb8f3e8648e7dd09c6059229876ab637a1d2
Zinko17 <vovazin2@gmail.com>
2021-07-06 17:27:37 +0600

    add unique in Day model

--- stom_project/signup/models.py
+++ stom_project/signup/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Day(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=15,unique=True)
 
     def __str__(self):
         return self.name
